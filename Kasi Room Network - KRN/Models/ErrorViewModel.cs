14dd5ee58cca79c94fb0d65d7da999d9ac08f5bc
iamunique014 <alungileunique@gmail.com>
2025-12-06 01:35:27 +0200

    Add project files.

---
+++ Kasi Room Network - KRN/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Kasi_Room_Network___KRN.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
