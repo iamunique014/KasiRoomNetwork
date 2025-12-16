c77e4facbfa64f8e66622325ba1a3633fbc5633a
iamunique014 <alungileunique@gmail.com>
2025-12-16 06:36:49 +0200

    Add:ListingSearchViewModel

---
+++ KasiRoomNetwork.Common/ViewModel/Listings/ListingSearchViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Common.ViewModel.Listings
+{
+    public class ListingSearchViewModel
+    {
+        // Filters
+        public string? Province { get; set; }
+        public string? City { get; set; }
+        public string? Suburb { get; set; }
+
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+
+        // Results
+        public List<ListingSearchResultViewModel> Results { get; set; } = new();
+    }
+}
