c95428fd1a1aae09cb8e461ae38cc5bbc11d2290
iamunique014 <alungileunique@gmail.com>
2025-12-16 06:30:16 +0200

    Add:CreateListingViewModel

---
+++ KasiRoomNetwork.Common/ViewModel/Listings/CreateListingViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Common.ViewModel.Listings
+{
+    public class CreateListingViewModel
+    {
+        // Address
+        [Required]
+        public string Province { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Suburb { get; set; }
+
+        public string? Street { get; set; }
+
+        // Listing
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0, 100000)]
+        public decimal Price { get; set; }
+    }
+}
