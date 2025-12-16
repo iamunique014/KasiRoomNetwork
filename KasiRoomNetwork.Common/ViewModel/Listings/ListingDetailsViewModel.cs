5cd9c792b4402fc570177dd51d62ff70caa66457
iamunique014 <alungileunique@gmail.com>
2025-12-16 06:33:56 +0200

    Add:ListingDetailsViewModel

---
+++ KasiRoomNetwork.Common/ViewModel/Listings/ListingDetailsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Common.ViewModel.Listings
+{
+    public class ListingDetailsViewModel
+    {
+        public int ListingId { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+        public bool IsVerified { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        // Address
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string Suburb { get; set; }
+        public string? Street { get; set; }
+
+        // Landlord
+        public string LandlordUserId { get; set; }
+        public string LandlordEmail { get; set; }
+
+        // Photos
+        public List<ListingPhotoViewModel> Photos { get; set; } = new();
+    }
+}
