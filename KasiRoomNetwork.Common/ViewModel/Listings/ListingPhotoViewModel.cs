949b3459b870bb399dac7aa702229117e32150dd
iamunique014 <alungileunique@gmail.com>
2025-12-16 06:33:32 +0200

    Add:ListingPhotoViewModel

---
+++ KasiRoomNetwork.Common/ViewModel/Listings/ListingPhotoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Common.ViewModel.Listings
+{
+    public class ListingPhotoViewModel
+    {
+        public int PhotoId { get; set; }
+        public string PhotoPath { get; set; }
+        public bool IsPrimary { get; set; }
+    }
+}
