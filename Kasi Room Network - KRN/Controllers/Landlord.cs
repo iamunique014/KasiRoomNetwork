854c5b2b188efe13137f037a233cabc286183845
iamunique014 <alungileunique@gmail.com>
2025-12-16 15:57:22 +0200

    Add: Landlord controller

---
+++ Kasi Room Network - KRN/Controllers/Landlord.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kasi_Room_Network___KRN.Controllers
+{
+    public class Landlord : Controller
+    {
+        public IActionResult start()
+        {
+            return View();
+        }
+    }
+}
