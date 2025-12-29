b41acebc8b28dfa8785014cc58714558df9784cf
iamunique014 <alungileunique@gmail.com>
2025-12-29 18:49:17 +0200

    feat: Role based routing after login

--- Kasi Room Network - KRN/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Kasi Room Network - KRN/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -21,11 +21,13 @@ namespace Kasi_Room_Network___KRN.Areas.Identity.Pages.Account
     public class LoginModel : PageModel
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<ApplicationUser> signInManager, ILogger<LoginModel> logger)
+        public LoginModel(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, ILogger<LoginModel> logger)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
             _logger = logger;
         }
 
@@ -116,6 +118,25 @@ namespace Kasi_Room_Network___KRN.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
+
+                    var user = await _userManager.FindByEmailAsync(Input.Email);
+                    if (user != null)
+                    {
+                        var roles = await _userManager.GetRolesAsync(user);
+                        if (roles.Contains("Admin"))
+                        {
+                            return RedirectToAction("AdminDashboard", "Admin");
+                        }
+                        else if (roles.Contains("Landlord"))
+                        {
+                            return RedirectToAction("LandlordDashboard", "Landlord");
+                        }
+                        else if (roles.Contains("Tenant"))
+                        {
+                            return RedirectToAction("TenantDashboard", "Tenant");
+                        }
+                    }
+
                     return LocalRedirect(returnUrl);
                 }
                 if (result.RequiresTwoFactor)
