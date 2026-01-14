0e0db7346a150c58f51143f11b984f92cd40da7b
iamunique014 <alungileunique@gmail.com>
2026-01-14 10:58:48 +0200

    Add: Admin Interface

---
+++ KasiRoomNetwork.Data/Interfaces/IAdminRepository.cs
@@ -0,0 +1,26 @@
+﻿using KasiRoomNetwork.Common.ViewModel.Admin;
+using KasiRoomNetwork.Common.ViewModel.Listings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Data.Interfaces
+{
+    public interface IAdminRepository
+    {
+        // ===== Listings Verification =====
+
+        Task<IEnumerable<UnverifiedListingViewModel>> GetUnverifiedListingsAsync();
+
+        Task<AdminListingReviewViewModel> GetListingForVerificationAsync(int listingId);
+
+        Task<IEnumerable<ListingPhotoViewModel>> GetListingPhotosAsync(int listingId);
+
+        Task VerifyListingAsync(int listingId, string adminUserId, bool isApproved, string notes );
+
+        // ===== Audit Logs =====
+        Task<IEnumerable<AdminVerificationLogViewModel>> GetVerificationLogsAsync(string entityType, int entityId);
+    }
+}
