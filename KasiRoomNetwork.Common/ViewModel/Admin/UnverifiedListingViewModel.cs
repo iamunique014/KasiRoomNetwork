0c17cd9b4d11cc879d029558f009bf5ca97a84ab
iamunique014 <alungileunique@gmail.com>
2026-01-14 11:00:31 +0200

    Add: Admin Repository  ViewModels

---
+++ KasiRoomNetwork.Common/ViewModel/Admin/UnverifiedListingViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Common.ViewModel.Admin
+{
+    public class UnverifiedListingViewModel
+    {
+        public int ListingId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string Suburb { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
