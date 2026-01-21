de01f1f504ac25958c05865846a7d59f415586b0
iamunique014 <alungileunique@gmail.com>
2026-01-21 12:02:59 +0200

    Add: ILandlord interface

---
+++ KasiRoomNetwork.Data/Interfaces/ILandlordRepository.cs
@@ -0,0 +1,14 @@
+﻿using KasiRoomNetwork.Common.ViewModel.Landlord;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Data.Interfaces
+{
+    public interface ILandlordRepository
+    {
+        Task<IEnumerable<LandlordListingViewModel>> GetAllLandlordListings(String landlordId);
+    }
+}
