55207cd8974bf5354e7c6f477e7517f8a1fae6b6
iamunique014 <alungileunique@gmail.com>
2026-01-21 12:02:17 +0200

    Add: LandlordListingViewModel

---
+++ KasiRoomNetwork.Common/ViewModel/Landlord/LandlordListingViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Common.ViewModel.Landlord
+{
+    public class LandlordListingViewModel
+    {
+        public int ListingId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public bool IsVerified { get; set; }
+
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string Suburb { get; set; }
+
+        public string? PrimaryPhoto { get; set; }
+    }
+}
