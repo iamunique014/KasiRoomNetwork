536be502f5a793d3576c289b8293c766ac37deee
iamunique014 <alungileunique@gmail.com>
2026-01-21 12:03:57 +0200

    Add: Implement ILandlord interface

---
+++ KasiRoomNetwork.Data/Repositories/LandlordRepository.cs
@@ -0,0 +1,34 @@
+﻿using KasiRoomNetwork.Common.ViewModel.Landlord;
+using KasiRoomNetwork.Common.ViewModel.Listings;
+using KasiRoomNetwork.Data.DataAccess;
+using KasiRoomNetwork.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Data.Repositories
+{
+    public class LandlordRepository : ILandlordRepository
+    {
+        private readonly ISqlDataAccess _db;
+
+        public LandlordRepository(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+        // ===============================
+        // All Landlord Listing
+        // ===============================
+        public async Task<IEnumerable<LandlordListingViewModel>> GetAllLandlordListings(string landlordId)
+        {
+            var result = await _db.GetData<LandlordListingViewModel, dynamic>(
+                "sp_Landlord_Get_All_Listings",
+                new { LandlordId = landlordId }
+            );
+
+            return result.ToList();
+        }
+    }
+}
