5c32fffaba8b0c762042242d8420b8c82bd236f7
iamunique014 <alungileunique@gmail.com>
2026-01-26 11:17:46 +0200

    Add: ManageUsersViewModel

---
+++ KasiRoomNetwork.Common/ViewModel/Admin/ManageUsersViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Common.ViewModel.Admin
+{
+    public class ManageUsersViewModel
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string RoleName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
