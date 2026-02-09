6ee8a102c70ce8187996c805290532cb264e6344
iamunique014 <alungileunique@gmail.com>
2026-02-09 16:03:49 +0200

    Add: ProfileViewModel

---
+++ KasiRoomNetwork.Common/ViewModel/Profiles/ProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Common.ViewModel.Profiles
+{
+    public class ProfileViewModel
+    {
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+    }
+
+}
