34ea0846cf6ff71515df86ba7acb065945501586
iamunique014 <alungileunique@gmail.com>
2026-02-09 16:05:02 +0200

    Add: IProfileRepository

---
+++ KasiRoomNetwork.Data/Interfaces/IProfileRepository.cs
@@ -0,0 +1,17 @@
+﻿using KasiRoomNetwork.Common.ViewModel.Profiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Data.Interfaces
+{
+    public interface IProfileRepository
+    {
+        Task<ProfileViewModel?> GetByUserId(string userId);
+        Task SaveProfile(ProfileViewModel profile, string userId);
+        Task<LandlordPublicProfileViewModel?> GetLandlordPublic(string userId);
+    }
+
+}
