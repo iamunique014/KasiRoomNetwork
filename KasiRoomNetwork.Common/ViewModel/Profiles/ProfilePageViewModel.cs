e9d9176f90a32cae0982312f6c366fa48f8c1bce
iamunique014 <alungileunique@gmail.com>
2026-02-20 10:16:00 +0200

    Add: ProfilePageViewModel

---
+++ KasiRoomNetwork.Common/ViewModel/Profiles/ProfilePageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Common.ViewModel.Profiles
+{
+    public class ProfilePageViewModel
+    {
+        public ProfileViewModel UserProfile { get; set; } = new();
+        public LandlordPublicProfileViewModel? LandlordProfile { get; set; }
+    }
+}
