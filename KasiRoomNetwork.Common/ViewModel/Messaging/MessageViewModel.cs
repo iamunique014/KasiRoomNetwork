f79e2c445a578dd19a31711c186f02d9db354ef2
iamunique014 <alungileunique@gmail.com>
2026-03-08 11:56:53 +0200

    Add: Messaging ViewModels

---
+++ KasiRoomNetwork.Common/ViewModel/Messaging/MessageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Common.ViewModel.Messaging
+{
+    public class MessageViewModel
+    {
+        public int MessageId { get; set; }
+        public int ConverstionId { get; set; }
+        public string SenderId { get; set; }
+        public string MessageText { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
