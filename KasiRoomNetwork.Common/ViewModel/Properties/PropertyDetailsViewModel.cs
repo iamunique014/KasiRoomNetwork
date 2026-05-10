cfc0e3587c6d9dc29f963ea6bb9ccece8495ac12
iamunique014 <alungileunique@gmail.com>
2026-05-10 06:10:28 +0200

    Add: Property deatils and photo viewModel

---
+++ KasiRoomNetwork.Common/ViewModel/Properties/PropertyDetailsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using KasiRoomNetwork.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasiRoomNetwork.Common.ViewModel.Properties
+{
+    public class PropertyDetailsViewModel
+    {
+        public int PropertyId { get; set; }
+
+        public string PropertyName { get; set; }
+        public int TotalRooms { get; set; }
+
+        public string PropertyType { get; set; }
+        
+        //public bool IsVerified { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        // Address
+        public string Province { get; set; }
+        public string City { get; set; }
+        public string Suburb { get; set; }
+        public string? Street { get; set; }
+
+        // Landlord
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string LandlordUserId { get; set; }
+
+        // Photos
+        public List<PropertyPhotoViewModel> Photos { get; set; } = new();
+
+        //Amenities
+        public List<AmenityModel> Amenities { get; set; } = new();
+    }
+}
