d94a9bdb2df87a8d5df9ee92498baf59ae0ac689
iamunique014 <alungileunique@gmail.com>
2026-06-09 06:06:53 +0200

    proc: Admin get listing for verification

---
+++ KasiRoomNetwork.Data/Database/StoredProcedures/sp_Admin_Get_Listing_For_Verification.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[sp_Admin_Get_Listing_For_Verification]
+    @ListingId INT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    SELECT 
+        l.ListingId,
+        l.Title,
+        l.Description,
+        l.Price,
+        l.IsVerified,
+        l.CreatedAt,
+        u.UserName AS LandlordName,
+        u.PhoneNumber,
+        a.Province,
+        a.City,
+        a.Suburb,
+        a.Street
+    FROM Listing l
+    INNER JOIN AspNetUsers u ON l.LandlordUserId = u.Id
+    INNER JOIN Property pr ON l.PropertyId = pr.PropertyId
+	INNER JOIN Address a ON pr.AddressId = a.AddressId
+    WHERE l.ListingId = @ListingId;
+END
