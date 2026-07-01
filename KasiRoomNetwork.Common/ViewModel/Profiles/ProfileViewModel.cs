6f75e8f90e9dd7ef83dfb32b216cfb02b535f9ff
iamunique014 <alungileunique@gmail.com>
2026-07-01 12:37:53 +0200

    Display validation for required fields

--- KasiRoomNetwork.Common/ViewModel/Profiles/ProfileViewModel.cs
+++ KasiRoomNetwork.Common/ViewModel/Profiles/ProfileViewModel.cs
@@ -9,7 +9,7 @@ namespace KasiRoomNetwork.Common.ViewModel.Profiles
         public string FullName { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Phone number is required.")]
-        [RegularExpression(@"^(?:\+27|0)[6-8][0-9]{8}$", ErrorMessage = "Please enter a valid South African phone number.")]
+        [RegularExpression(@"^(?:\+27|0)\s?(6|7|8)\d{1}\s?\d{3}\s?\d{4}$", ErrorMessage = "Please enter a valid South African phone number.")]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; } = string.Empty;
 
